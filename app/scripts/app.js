6a324f8cff8050b7cac47cdbf21d2676ca002c8a
nicholasxjy <yitaqi2011@gmail.com>
2015-01-14 23:02:30 +0800

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularFirebaseApp
+ * @description
+ * # angularFirebaseApp
+ *
+ * Main module of the application.
+ */
+angular.module('angularFirebaseApp', [
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
